b03aa568c6665c3842cb36b83993b26bc80c82d1
Josh Lee <joshua.huggins@sbcglobal.net>
2024-02-20 16:20:39 -0800

    added the function to start and run the quiz

--- assets/questions.js
+++ assets/questions.js
@@ -3,46 +3,46 @@ var questions = [
     { 
         question: " How to write a comment in Javascript?", 
         answers: [
-            { text: "<!--This is a comment-->", correct: false },
-            { text: "This is a comment" , correct: false },
-            { text: "/* This is a comment */", correct: false },
-            { text: "// This is a comment", correct: true }
+            { text: "A. <!--This is a comment-->", correct: false },
+            { text: "B. This is a comment" , correct: false },
+            { text: "C. /* This is a comment */", correct: false },
+            { text: "D. // This is a comment", correct: true }
         ]
     },
     { 
         question: "Which method is used to sorts the elements of an array?", 
         answers: [
-            { text: "concat()", correct: false },
-            { text: "order()", correct: false },
-            { text: "sort()", correct: true },
-            { text: "changeOrder()", correct: false }
+            { text: "A. concat()", correct: false },
+            { text: "B. order()", correct: false },
+            { text: "C. sort()", correct: true },
+            { text: "D. changeOrder()", correct: false }
         ]
     },
     { 
         question: "Which event is related to the keyboard?", 
         answers: [
-            { text: "onkeypress", correct: true },
-            { text: "onfocus", correct: false },
-            { text: "onclick", correct: false },
-            { text: "hover", correct: false }
+            { text: "A. onkeypress", correct: true },
+            { text: "B. onfocus", correct: false },
+            { text: "C. onclick", correct: false },
+            { text: "D. hover", correct: false }
         ]
     },
     { 
         question: "which of the method is used to get HTML element in javascript?", 
         answers: [
-            { text: "getElementbyId()", correct: false },
-            { text: "Both A & C", correct: true },
-            { text: "getElementsByClassName()", correct: false },
-            { text: "None of the above", correct: false }
+            { text: "A. getElementbyId()", correct: false },
+            { text: "B. Both A & C", correct: true },
+            { text: "C. getElementsByClassName()", correct: false },
+            { text: "D. None of the above", correct: false }
         ]
     },
     { 
         question: "Which of the keyword is used to define the variable in the javascript",
         answers: [
-            { text: "Var", correct: false },
-            { text: "let", correct: false },
-            { text: "Both A & B", correct: true },
-            { text: "None of the above", correct: false }
+            { text: "A. Var", correct: false },
+            { text: "B. let", correct: false },
+            { text: "C. Both A & B", correct: true },
+            { text: "D. None of the above", correct: false }
         ]
     },
 ];
